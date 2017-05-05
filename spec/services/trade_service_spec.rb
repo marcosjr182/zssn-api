1c0209ec2217472cb3ea134ca9d1fd6adb1cd97c
Marcos Siqueira Jr <junior.crvg@gmail.com>
2017-05-05 10:43:28 -0300

    Expand tests on TradeService

--- spec/services/trade_service_spec.rb
+++ spec/services/trade_service_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe TradeService, type: :service do
   let!(:survivor)  { create(:survivor) }
+  let!(:infected)  { create(:survivor, infected: true) }
   let!(:recipient) { create(:survivor) }
   let(:offer_items) { { water: 0, food: 0, medication: 0, ammo: 0 } }
   let(:trade_params) do
@@ -20,6 +21,22 @@ describe TradeService, type: :service do
       it { expect(recipient.inventory.reload[:water]).to eq(0) }
     end
 
-  end
+    context "when a trade has an invalid score" do
+      let!(:invalid_score_params) { trade_params.merge(survivor: { id: survivor.id, offer: offer_items.merge(ammo: 1) }) }
+      subject { TradeService.new(survivor, recipient, invalid_score_params) }
+      it { expect { subject.process }.to raise_error(TradeService::InvalidTrade) }
+    end
 
+    context "when a trader has not enough items to trade" do
+      let!(:invalid_balance_params) { trade_params.merge(survivor: { id: survivor.id, offer: offer_items.merge(ammo: 10) }) }
+      subject { TradeService.new(survivor, recipient, invalid_balance_params) }
+      it { expect { subject.process }.to raise_error(TradeService::InvalidTrade) }
+    end
+
+    context 'when a trader is infected' do
+      let(:infected_params) { trade_params.merge(survivor: { id: infected.id, offer: offer_items.merge(medication: 2) }) }
+      subject { TradeService.new(survivor, infected, infected_params) }
+      it { expect { subject.process }.to raise_error(TradeService::InvalidTrade) }
+    end
+  end
 end
